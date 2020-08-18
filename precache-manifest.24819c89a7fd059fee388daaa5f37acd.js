ced828837294f19ba99f3edc250d87c724b87ce3
reihan97 <perreyhan96@yahoo.com>
2020-08-18 20:12:37 +0300

    Updates

---
+++ precache-manifest.24819c89a7fd059fee388daaa5f37acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd9c2e67822b2fc88c16eeb9c6229b3",
+    "url": "/resume-portfolio/index.html"
+  },
+  {
+    "revision": "1907d9d70741a4efc3af",
+    "url": "/resume-portfolio/static/css/main.caf53763.chunk.css"
+  },
+  {
+    "revision": "0f7722abe494acf9f006",
+    "url": "/resume-portfolio/static/js/2.17276e48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume-portfolio/static/js/2.17276e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1907d9d70741a4efc3af",
+    "url": "/resume-portfolio/static/js/main.064fa9ed.chunk.js"
+  },
+  {
+    "revision": "5a0568ec53de4bac1a4a",
+    "url": "/resume-portfolio/static/js/runtime-main.bda87ca9.js"
+  }
+]);
